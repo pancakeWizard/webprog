c7ef92da8cd16b62bdbba0658d4395dcea41fea6
Vertushka <43088904+Vertuushka@users.noreply.github.com>
2024-03-15 09:48:04 +0100

    book page render

--- bookprj/forum/views.py
+++ bookprj/forum/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from . models import *
 
 # Create your views here.
+def books(request):
+    books = Book.objects.all()
+    data = {
+        "navBookBtn": True,
+        "books": books
+    }
+    return render(request, 'books.html', data)
--- bookprj/home/views.py
+++ bookprj/home/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def homepage(request):
-    return render(request, 'landing.html')
+    return render(request, 'landing.html', {"navMainBtn":True})
